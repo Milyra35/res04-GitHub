bf6113cc0015ac744c09ae86c8fbe399ec9c184d
Milyra35 <marie.richir35@gmail.com>
2023-06-21 11:28:21 +0200

    création des cases colorées

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", function() {
+    console.log("hello");
+
+    let boxes = document.querySelectorAll("main div");
+
+    for (box of boxes)
+    {
+
+    }
+})
